3d1273d3725435f9471c955d5ce9ba2acc6d201b
Joe Rixon <joerixon8@gmail.com>
2026-08-07 13:52:14 +0200

    Oh my days what a large template

--- web/static/static.go
+++ web/static/static.go
@@ -4,5 +4,5 @@ import "embed"
 
 // FS contains the static assets served by Songdock.
 //
-//go:embed songdock_logo_dark.v2.png song_artwork_placeholder.png lucide.svg favicon/*
+//go:embed songdock_logo_dark.v2.png song_artwork_placeholder.png lucide.svg platformadmin.v1.css admin-auth.v1.css admin-base.v1.css admin-home.v1.css admin-song-form.v1.css favicon/*
 var FS embed.FS
